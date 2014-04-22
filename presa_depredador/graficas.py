1de641f352097445baa09020f4730f15f94b41e2
henrymmg <hm.martinez57@uniandes.edu.co>
2014-04-22 11:38:46 -0500

    punto uno final

--- presa_depredador/graficas.py
+++ presa_depredador/graficas.py
@@ -5,10 +5,11 @@ datos = np.loadtxt('datos.txt')
 for i in range(30):
     x=datos[i*1000:((i*1000)+999),0]
     y=datos[i*1000:((i*1000)+999),1]
-    plt.plot(x,y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('X(0)='+str(30-i)+'\\Y(0)='+str(datos[0,1]))
+    plt.scatter(x,y)
+    plt.xlabel('Presas')
+    plt.ylabel('Cazadores')
+    plt.title('Presas iniciales:'+str(30-i)+', Cazadores iniciales:'+str(int(datos[0,1])))
     plt.savefig('presa_depredador'+str(30-i)+'.png',dpi=200)
+    plt.show()
 
 
--- presa_depredador/volterra_lotka.c
+++ presa_depredador/volterra_lotka.c
@@ -6,15 +6,33 @@
 #define C 30.0
 #define D 1.0
 
+float x_prima(float T,float X,float Y);
+float y_prima(float T,float X,float Y);
+float RK4(int puntos,float h);
+
 int main(){
 
   /*Definicion variables*/
-  int i;
   float h;
   float t_min;
   float t_max;
   float n_puntos;
   int puntos;
+    
+  /*Inicializacion variables*/
+  h=0.001; //Paso de iteracion
+  t_min=0.0;  //Rango de tiempo
+  t_max=1.0;
+  n_puntos=(t_max-t_min)/h;  //Numero de puntos
+  puntos=(int)n_puntos+1;
+  
+  RK4(puntos,h); //Funcion que resuelve las ecuaciones diferenciales por el metodo de Runge-Kutta de cuarto orden
+  return 0;
+}
+
+/*Funcion Runge Kutta de cuarto orden*/
+float RK4(int puntos,float h){
+  int i;
   float k1x;
   float k1y;
   float t1;
@@ -35,38 +53,18 @@ int main(){
   float k_medio_x;
   float k_medio_y;
   FILE *in;
-  
-  /*Inicializacion variables*/
-  h=0.001; //Paso de iteracion
-  t_min=0.0;  //Rango de tiempo
-  t_max=1.0;
-  n_puntos=(t_max-t_min)/h;  //Numero de puntos
-  puntos=(int)n_puntos+1;
-  printf("%d\n",puntos);
   float t[puntos];  //Variable independiente
   float x[puntos];  //Variables dependientes
   float y[puntos];
-  in=fopen("datos.txt","w");
-
-  /*Funcion x'(t)*/
-  float x_prima(T,X,Y){
-    float x_punto;
-    x_punto= A*X-B*X*Y;
-    return x_punto;
-  }
-
-  /*Funcion y'(t)*/
-  float y_prima(T,X,Y){
-    float y_punto;
-    y_punto= -C*Y+D*X*Y;
-    return y_punto;
-  }
   
   /*Condiciones iniciales X(0) y Y(0) para  X'(0)=Y'(0)=0*/
   t[0]=0;  //Tiempo inicial
   y[0]=A/B; //Al evaluar X'(0)=0 se puede cancelar X(0) en la ecuacion
   x[0]=C/D; //Al evaluar Y'(0)=0 se puede cancelar Y(0) en la ecuacion
-
+  
+  /*Archivo en el que se van a guardar los datos*/  
+  in=fopen("datos.txt","w");
+  
   do{
   /*Solucion del conjunto de ecuaciones diferenciales variando el valor inicial de X*/
   for (i=1;i<puntos;i++){
@@ -98,17 +96,34 @@ int main(){
       k_medio_x=(1.0/6.0)*(k1x+2.0*k2x+2.0*k3x+k4x);
       k_medio_y=(1.0/6.0)*(k1y+2.0*k2y+2.0*k3y+k4y);
     
+      /*Variables con valores finales*/
       t[i]=t[i-1]+h;
       x[i]=x[i-1]+h*k_medio_x;
       y[i]=y[i-1]+h*k_medio_y;
+     
+      /*Se guardan los datos en el archivo de texto*/
       fprintf(in,"%f %f\n",x[i],y[i]);
   }
-  printf("%f\n",x[0]);
+
   x[0]=x[0]-1;
   }while(x[0]>0);
 
   fclose(in);
-
-return 0;
-
 }
+  
+  /*Funcion x'(t)*/
+  float x_prima(float t,float x,float y){
+    float x_punto;
+    x_punto= A*x-B*x*y;
+    return x_punto;
+  }
+  
+  /*Funcion y'(t)*/
+  float y_prima(float t,float x,float y){
+    float y_punto;
+    y_punto= -C*y+D*x*y;
+    return y_punto;
+  }
+  
+
+
